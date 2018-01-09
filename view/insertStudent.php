fcfd869b9958142a38d8989cd980319934ad5c66
kevinliposl <kevinliposl@gmail.com>
2018-01-09 16:49:04 -0600

    asd

--- view/insertStudent.php
+++ view/insertStudent.php
@@ -5,47 +5,57 @@ include_once '../public/headerIN.php';
 
 <form method="POST" onsubmit="return false;" action="../business/StudentBusiness.php">
     <div>
-        <label for="name">Correo:</label>
-        <input type="text" id="form-mail" name="form-mail" minlength="4" required/>
+        <label for="mail">Correo*</label>
+        <input type="email" id="form-mail" name="form-mail" minlength="4" required/>
     </div>
     <div>
-        <label for="name">Nombre:</label>
+        <label for="name">Nombre*</label>
         <input type="text" id="form-name" name="form-name" minlength="4" required/>
     </div>
     <div>
-        <label for="name">Primer Apellido:</label>
+        <label for="lastname1">Primer Apellido*</label>
         <input type="text" id="form-lastname1" name=form-lastname1" minlength="4" required/>
     </div>
     <div>
-        <label for="name">Segundo Apellido:</label>
-        <input type="text" id="form-lastname2" name="form-lastname2"minlength="4" required/>
+        <label for="lastname2">Segundo Apellido*</label>
+        <input type="text" id="form-lastname2" name="form-lastname2" minlength="4" required/>
     </div>
     <div>
-        <label for="name">Primer Carrera:</label>
-        <input type="text" id="form-lastname2" name="form-lastname2"minlength="4" required/>
+        <label for="career1">Primer Carrera*</label>
+        <input type="text" id="form-career1" name="form-career1" minlength="4" required/>
     </div>
     <div>
-        <label for="name">Segunda Carrera:</label>
-        <input type="text" id="form-lastname2" name="form-lastname2"minlength="4"/>
+        <label for="career2">Segunda Carrera</label>
+        <input type="text" id="form-career2" name="form-career2" minlength="4"/>
     </div>
     <div>
-        <label for="name">Sede:</label>
-        <input type="text" id="form-lastname2" name="form-lastname2"minlength="4"/>
+        <label for="headquarters">Sede*</label>
+        <input type="text" id="form-headquarters" name="form-headquarters" minlength="4" required/>
     </div>
     <div>
         <input type="submit" id="form-submit"/>
     </div>
     <div id="state"></div>
 </form>
-
-
 <script>
     $("#form-submit").click(function () {
-        
-        
-        
-        
-        
+
+        var args = {
+            "mail": $("#form-mail").val().trim(),
+            "name": $("#form-name").val().trim(),
+            "lastname1": $("#form-lastname1").val().trim(),
+            "lastname2": $("#form-lastname2").val().trim(),
+            "career1": $("#form-career1").val().trim(),
+            "career2": $("#form-career2").val().trim(),
+            "headquarters": $("#form-headquarters").val().trim(),
+            "create": "create"
+        };
+        alert(JSON.stringify(args));
+        $("#state").text("Espere un momento...");
+
+        $.post('../business/StudentBusiness.php', args, function (data) {
+            $("#state").text(data.toString());
+        }, 'json');
     });
 </script>
 <?php
