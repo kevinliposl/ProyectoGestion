95fbd8091328d2dec8d69db06190d30573bc8f8d
dylan <dylan@dylan-Inspiron-3437>
2018-01-13 23:52:07 -0600

    Agrega la clase university en el domain

---
+++ domain/University.php
@@ -0,0 +1,55 @@
+<?php
+
+class University {
+    
+    //Attributes
+    private $universityid;
+    private $universitycode;
+    private $universityname;
+    private $universityType;
+    
+    function __construct() {
+        ;
+    }//End construct()
+    
+    function __toString() {
+        try {
+            return (string) $this->universityid . ' | ' . $this->universitycode . ' | ' . $this->universityname . ' | ' . $this->universityType;
+        } catch (Exception $e) {
+            return '';
+        }//ENd try-catch (Exception $e)
+    }//ENd yoString()
+    
+    function setUniversityid($universityid) {
+        $this->id = $universityid;
+    }//End setUniversityid()
+    
+     function setUniversitycode($universitycode) {
+        $this->universitycode = $universitycode;
+    }//End setUniversitycode()
+    
+    function setUniversityname($universityname) {
+        $this->universityname = $universityname;
+    }//End setUniversityname()
+    
+    function setUniversityType($universityType) {
+        $this->universityType = $universityType;
+    }//End setUniversityType()
+    
+    function getUniversityid() {
+        return $this->universityid;
+    }//End getUniversityid()    
+    
+    function getUniversityCode(){
+        return $this->universitycode;
+    }//End getUniversityCode()
+    
+    function getUniversityName(){
+        return $this->universityname;
+    }//End getUniversityName()
+    
+    function getUniversityType(){
+        return $this->universityType;
+    }//End getUNiversityType()
+    
+}//End class University
