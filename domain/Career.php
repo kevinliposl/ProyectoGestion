5030c1d5ca75799c024d7be0f756185149c4192b
dylan <dylan@dylan-Inspiron-3437>
2018-01-14 00:14:49 -0600

    Agrega la clase Career al domain

---
+++ domain/Career.php
@@ -0,0 +1,47 @@
+<?php
+
+
+class Career {
+    
+    //Attributes
+    private $careerid;
+    private $careercode;
+    private $careername;
+    
+    function __construct() {
+        ;
+    }//End construct()
+    
+    function __toString() {
+        try {
+            return (string) $this->careerid . ' | ' . $this->careercode . ' | ' . $this->careername;
+        } catch (Exception $e) {
+            return '';
+        }//End try-catch (Exception $e)
+    }//End toString()
+    
+    function setCareerid($careerid) {
+        $this->careerid = $careerid;
+    }//End setCareerid()
+    
+     function setCareercode($careercode) {
+        $this->careercode = $careercode;
+    }//End setCareercode()
+    
+    function setCareername($careername) {
+        $this->careername = $careername;
+    }//End setCareername()
+    
+    function getCareerid() {
+        return $this->careerid;
+    }//End getCareerid()    
+    
+    function getCareercode(){
+        return $this->careercode;
+    }//End getCareercode()
+    
+    function getCareername(){
+        return $this->careername;
+    }//End getCareername()
+    
+}//End class Career
