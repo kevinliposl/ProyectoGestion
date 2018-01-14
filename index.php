e2f61de4d949664b3c86826e0379357325dd27b8
dylan <dylan@dylan-Inspiron-3437>
2018-01-14 12:28:01 -0600

    soluciona la falta del /view en el index

--- index.php
+++ index.php
@@ -19,9 +19,9 @@
                                
                                 <li><a href="#"><div>Universidades</div></a>
                                     <ul>
-                                        <li><a href="careerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
-                                        <li><a href="universityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
-                                        <li><a href="HeadquarterView.php"><div>Gesti&oacute;n de Recintos</div></a></li>
+                                        <li><a href="view/careerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
+                                        <li><a href="view/universityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
+                                        <li><a href="view/HeadquarterView.php"><div>Gesti&oacute;n de Recintos</div></a></li>
                                     </ul>
                                 </li>
 			</ul>
