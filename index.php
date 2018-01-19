cd8571a7ff8db290043921662f21285e821fa1e2
kevinliposl <kevinliposl@gmail.com>
2018-01-19 16:15:27 -0600

    awd

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>ProyectoGesti&oacute;n</title>
-	<script src="public/js/jquery.min.js" type="text/javascript"></script>
-	<script src="public/js/global.js" type="text/javascript"></script>
-</head>
-<body>
-	<header>
-		<nav>
-			<ul>
-				<li><a href="index.php"><div>Home</div></a></li> 
-				<li><a href="#"><div>Usuarios</div></a>
-					<ul>
-						<li><a href="view/StudentView.php"><div>Gesti&oacute;n de Estudiantes</div></a></li>
-					</ul>
-				</li>
-                               
-                                <li><a href="#"><div>Universidades</div></a>
-                                    <ul>
-                                        <li><a href="view/CareerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
-                                        <li><a href="view/UniversityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
-                                        <li><a href="view/HeadquarterView.php"><div>Gesti&oacute;n de Recintos</div></a></li>
-                                    </ul>
-                                </li>
-			</ul>
-		</nav>
-	</header>
-	
-	<h1>INDEX</h1>
+    <head>
+        <meta charset="UTF-8">
+        <title>ProyectoGesti&oacute;n</title>
+        <script src="public/js/jquery.min.js" type="text/javascript"></script>
+        <script src="public/js/global.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="index.php"><div>Home</div></a></li> 
+                    <li><a href="#"><div>Usuarios</div></a>
+                        <ul>
+                            <li><a href="view/StudentView.php"><div>Gesti&oacute;n de Estudiantes</div></a></li>
+                        </ul>
+                    </li>
 
-</body>
+                    <li><a href="#"><div>Universidades</div></a>
+                        <ul>
+                            <li><a href="view/CareerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
+                            <li><a href="view/UniversityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
+                            <li><a href="view/HeadquarterView.php"><div>Gesti&oacute;n de Sedes y Recintos</div></a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <h1>INDEX</h1>
+
+    </body>
 </html>
--- view/HeadquarterView.php
+++ view/HeadquarterView.php
@@ -32,7 +32,7 @@ include_once '../public/header.php';
                 </select> 
             </td>
             <td>    
-                <select id="form-headquarters-enclosure" style="display: none">
+                <select id="form-select-headquarters-enclosure" style="display: none">
                     <option value="-1">
                         Seleccione un tipo
                     </option>
@@ -70,8 +70,7 @@ include_once '../public/header.php';
 <table id="form-headquarters" style="display: none;">
     <thead>
         <tr>
-            <th id="input-headquarters-text">Sede</th>
-            <th>Nombre</th>
+            <th>Sede</th>
         </tr>
     </thead>
     <form method="POST">
@@ -93,7 +92,52 @@ include_once '../public/header.php';
     </form>
 </table>
 
+<table id="form-only-enclosure" style="display: none;">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+        </tr>
+    </thead>
+    <form method="POST" onsubmit="submitOnlyEnclosure(); return false;">
+        <tr>
+            <td>
+                <input type="text" placeholder="Recinto" required/>
+            </td>
+            <td>
+                <input type="submit" value="Insertar"/>
+            </td>
+        </tr>
+    </form>
+</table>
+
+
 <script>
+
+    function submitOnlyEnclosure() {
+        var args = {
+
+        };
+
+        $.post('../business/EnclosureBusiness.php', args, function (data) {
+            if (data.result === "1") {
+
+            } else {
+
+            }
+        }, "json").fail(function () {
+            alert("La solicitud a fallado!!!");
+        });
+    }
+
+    function showHeadquarterAndEnclosure() {
+        $("#form-th").css("display", 'block');
+        $("#form-select-headquarters-enclosure").css("display", "block");
+    }
+
+    function showOnlyEnclosure() {
+        $("#form-only-enclosure").css("display", 'block');
+    }
+
     $("#form-university").change(function () {
         if ($("#form-university").val() !== "-1") {
             var args = {
@@ -102,42 +146,56 @@ include_once '../public/header.php';
             };
             $.post('../business/UniversityBusiness.php', args, function (data) {
                 if (data.universityhadheadquarter === "1") {
-                    $("#form-enclosure").css('display', 'none');
-                    $("#form-headquarters-enclosure").css("display", "block");
-                    $("#form-th").css("display", 'block');
+                    hideAll();
+                    showHeadquarterAndEnclosure();
                 } else {
-                    $("#input-headquarters-text").css('display','none');
-                    $("#input-headquarters").css('display','none');
-                    $("#form-th").css("display", 'none');
-                    $("#form-headquarters-enclosure").css("display", "none");
-                    $("#form-enclosure").css('display', 'none');
-                    $("#form-headquarters").css("display", "block");
+                    hideAll();
+                    showOnlyEnclosure();
                 }
             }, "json").fail(function () {
                 alert("La solicitud a fallado!!!");
             });
         } else {
-            $("#form-enclosure").css('display', 'none');
-            $("#form-th").css("display", 'none');
-            $("#form-headquarters-enclosure").css("display", "none");
-            $("#form-headquarters").css("display", "none");
+            hideAll();
         }
     });
-    $("#form-headquarters-enclosure").change(function () {
-        if ($("#form-headquarters-enclosure").val() === "1") {
-            $("#form-headquarters").css("display", "none");
 
+    $("#form-select-headquarters-enclosure").change(function () {
+        if ($("#form-select-headquarters-enclosure").val() === "1") {
+            $("#form-headquarters").css("display", "none");
             $("#form-enclosure").css("display", "block");
 
-        } else if ($("#form-headquarters-enclosure").val() === "0") {
+        } else if ($("#form-select-headquarters-enclosure").val() === "0") {
             $("#form-enclosure").css("display", "none");
             $("#form-headquarters").css("display", "block");
-
         } else {
-            $("#form-enclosure").css("display", "none");
-            $("#form-headquarters").css("display", "none");
+            hideAll();
         }
     });
+
+    function hideAll() {
+        hideSelectHeadquartersEnclosure();
+        hideFormEnclosure();
+        hideFormHeadquarters();
+        hideFormOnlyEnclosure();
+    }
+
+    function hideFormHeadquarters() {
+        $("#form-headquarters").css("display", "none");
+    }
+
+    function hideFormEnclosure() {
+        $("#form-enclosure").css("display", "none");
+    }
+
+    function hideFormOnlyEnclosure() {
+        $("#form-only-enclosure").css("display", "none");
+    }
+
+    function hideSelectHeadquartersEnclosure() {
+        $("#form-th").css("display", 'none');
+        $("#form-select-headquarters-enclosure").css("display", "none");
+    }
 
 </script>
 
