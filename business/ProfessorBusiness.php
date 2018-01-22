57f66f9b5ef3469baee56d24e8ab55ea9f54666b
kevinliposl <kevinliposl@gmail.com>
2018-01-22 10:51:23 -0600

    asd

--- business/ProfessorBusiness.php
+++ business/ProfessorBusiness.php
@@ -3,62 +3,58 @@
 require '../domain/Professor.php';
 
 if (isset($_POST['create'])) {
-    if ($_POST['action'] == "only") {
-        if (isset($_POST['name']) && isset($_POST['universityid'])) {
-            if (strlen($_POST['name']) > 0 && strlen($_POST['universityid']) > 0) {
+    if (isset($_POST['']) && isset($_POST[''])) {
+        if (strlen($_POST['']) > 0 && strlen($_POST['']) > 0) {
 
-                echo json_encode(array("result" => $result));
-            } else {
-                echo json_encode(array("result" => -1));
-            }
-        } else {
-            echo json_encode(array("result" => -2));
-        }
-    } else {
-        if (isset($_POST['enclosurename']) && isset($_POST['universityid']) && isset($_POST['headquarterid'])) {
-            if (strlen($_POST['enclosurename']) > 0 && strlen($_POST['universityid']) > 0 && strlen($_POST['headquarterid']) > 0) {
-                $enclosureBusiness = new EnclosureBusiness();
-                $enclosure = new Enclosure();
-                $enclosure->setEnclosurename($_POST['enclosurename']);
-                $enclosure->setEnclosureuniversityid($_POST['universityid']);
-                $enclosure->setEnclosureheadquarterid($_POST['headquarterid']);
-                $result = $enclosureBusiness->insert($enclosure);
 
-                echo json_encode(array("result" => $result)); //error or success
-            } else {
-                echo json_encode(array("result" => -1)); //format
-            }
-        } else {
-            echo json_encode(array("result" => -2)); // empty
-        }
-    }
-} else if (isset($_POST['delete'])) {
-    if (isset($_POST['enclosureid'])) {
-        if (strlen($_POST['enclosureid']) > 0) {
-            $enclosureBusiness = new EnclosureBusiness();
 
-            $enclosure = new Enclosure();
-            $enclosure->setEnclosureid($_POST['enclosureid']);
-            $result = $enclosureBusiness->delete($enclosure);
+
+
 
             echo json_encode(array("result" => $result));
         } else {
-            echo json_encode(array("result" => "error"));
+            echo json_encode(array("result" => -1));
         }
     } else {
-        echo json_encode(array("result" => "empty"));
+        echo json_encode(array("result" => -2));
+    }
+} else if (isset($_POST['delete'])) {
+    if (isset($_POST[''])) {
+        if (strlen($_POST['']) > 0) {
+
+
+
+
+            echo json_encode(array("result" => $result));
+        } else {
+            echo json_encode(array("result" => -1));
+        }
+    } else {
+        echo json_encode(array("result" => -2));
     }
 } else if (isset($_POST['update'])) {
-    
-}
+    if (isset($_POST[''])) {
+        if (strlen($_POST['']) > 0) {
 
+
+
+
+            echo json_encode(array("result" => $result));
+        } else {
+            echo json_encode(array("result" => -1));
+        }
+    } else {
+        echo json_encode(array("result" => -2));
+    }
+}
 
 class ProfessorBusiness {
 
     private $data;
 
     function __construct() {
-        ;
+        include_once '../data/';
+        $this->data = new ();
     }
 
     function insert(Professor $professor) {
