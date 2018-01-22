e9987a776087a30ea7aba7189227c616417ac800
kevinliposl <kevinliposl@gmail.com>
2018-01-22 09:10:52 -0600

    awd

--- data/SPDO.php
+++ data/SPDO.php
@@ -9,7 +9,6 @@ class SPDO extends PDO {
             ///Para la base de datos de Jonathan
             parent::__construct('mysql:host=' . '127.0.0.1' . ';dbname=' . 'ucrpagecenter', 'root', 'Red12345');
             parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            parent::setAttribute(MYSQL_ATTR_INIT_COMMAND, "SET NAMES utf8");
             parent::setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
         } catch (PDOException $e) {
             try {
