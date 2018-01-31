ffe82e01e4cd37a68c4d4e81cb303717e91b9e61
dylan <dylan@dylan-Inspiron-3437>
2018-01-31 04:13:21 -0600

    agrega el activityBusiness y el EventBusiness

--- business/ActivityBusiness.php
+++ business/ActivityBusiness.php
@@ -2,6 +2,25 @@
 
 class ActivityBusiness {
     
+    //Attributes
+    private $data;
+
+    function __construct() {
+        include_once '../data/ActivityData.php';
+        $this->data = new ActivityData();
+    }//End construct
+
+    function insert(Activity $activity) {
+        return $this->data->insert($activity);
+    }//End insert
+
+    function delete(Activity $activity) {
+        return $this->data->delete($activity);
+    }//End delete
+
+    function selectAll() {
+        return $this->data->selectAll();
+    }//End selectAll
     
-}//End class ActivityBusiness {
+}//End class ActivityBusiness 
     
