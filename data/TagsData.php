518339d24624ab8ea18e5672fb3ba17a0998b786
kevinliposl <kevinliposl@gmail.com>
2018-02-07 09:54:23 -0600

    awd

--- data/TagsData.php
+++ data/TagsData.php
@@ -1,6 +1,6 @@
 <?php
 
-class TagsData {
+class TagData {
 
     private $db;
 
@@ -10,53 +10,19 @@ class TagsData {
     }
 
     function insert(Tag $tag) {
-        $queryExistsMail = $this->db->prepare("SELECT tagword FROM tbtag WHERE tagactivityid=:activityid AND tagword=:word;");
-        $queryExistsMail->execute(array('tagactivityid' => $tag->gettagactivityid(), 'word' => $tag->gettagword()));
-        $resultMail = $queryExistsMail->fetch();
-        $queryExistsMail->closeCursor();
+        $queryExistsTag = $this->db->prepare("SELECT tagword FROM tbtag WHERE tagactivityid=:activityid AND tagword=:word;");
+        $queryExistsTag->execute(array('tagactivityid' => $tag->gettagactivityid(), 'word' => $tag->gettagword()));
+        $resultTag = $queryExistsTag->fetch();
+        $queryExistsTag->closeCursor();
 
-        if ($resultMail['actormail'] == NULL) {
-            $idStudent = $this->getLastId();
-            $queryInsertStudent = $this->db->prepare("INSERT INTO tbstudent VALUES (:id,:license,:name,:lastname1,:lastname2,:career1,:career2,:password,:state);");
-            $queryInsertStudent->execute(array('id' => $idStudent, 'license' => $student->getStudentlicense(), 'name' => $student->getStudentname(),
-                'lastname1' => $student->getStudentlastname1(), 'lastname2' => $student->getStudentlastname2(), 'career1' => $student->getStudentcareer1(),
-                'career2' => $student->getStudentcareer2(), 'password' => $student->getStudentpassword(), 'state' => 0));
-            $queryInsertStudent->fetch();
+        if ($resultTag['tagword'] == NULL) {
+            $queryInsertTag = $this->db->prepare("INSERT INTO tbtag VALUES (:idactivity,:word);");
+            $queryInsertTag->execute(array());
+            $queryInsertTag->fetch();
 
-            $queryInsertStudent->closeCursor();
-            $resultStudent = $this->select($idStudent);
-
-            if ($resultStudent->getStudentid() !== NULL) {
-                $queryInsertActor = $this->db->prepare("INSERT INTO tbactor VALUES (:actorid,:actormail);");
-                $queryInsertActor->execute(array('actorid' => $idStudent, 'actormail' => $student->getStudentmail()));
-                $resultActor = $queryInsertActor->fetch();
-                $queryInsertActor->closeCursor();
-
-                if (!$resultActor) {
-                    return 1;
-                } else {
-                    return 0;
-                }
-            } else {
-                return 0;
-            }
-        } else {
-            return 0;
-        }
-    }
-
-    function update(Student $student) {
-        $query = $this->db->prepare("UPDATE tbstudent s INNER JOIN tbactor a ON a.actorid = s.studentid "
-                . "SET s.studentlicense=:license, s.studentname=:name, s.studentlastname1=:lastname1, s.studentlastname2=:lastname2,"
-                . "s.studentpassword=:password, s.studentcareer1=:career, a.actormail=:mail WHERE s.studentid=:id;");
-        $query->execute(array("license" => $student->getStudentlicense(), "name" => $student->getStudentname(), "lastname1" => $student->getStudentlastname1(),
-            "lastname2" => $student->getStudentlastname2(), "password" => $student->getStudentpassword(), "id" => $student->getStudentid(),
-            "mail" => $student->getStudentmail(), "career" => $student->getStudentcareer1()));
-        $result = $query->fetch();
-        $query->closeCursor();
-
-        if (!$result) {
-            return 1;
+            $queryInsertTag->closeCursor();
+            
+            
         } else {
             return 0;
         }
