0741fdc14fc234649033cef358c79fde19533481
kevinliposl <kevinliposl@gmail.com>
2018-02-12 14:08:19 -0600

    asd

--- view/SearchView.php
+++ view/SearchView.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once '../public/header.php';
 ?>
 <table>
@@ -6,49 +7,63 @@ include_once '../public/header.php';
         <th>Busqueda General</th>
         <th>Fecha</th>
         <th>Tipo de Actividad</th>
+        <th>Lugar</th>
+        <th>Hora</th>
+        <th>Actor</th>
     </tr>
-    <form enctype="multipart/form-data" method='POST' action='../business/CommentBusiness.php'>
-        <tr>
-            <td>
-                <input type ='text' name="searchGeneral" placeholder="Busqueda General"/>
-            </td>
-            <td>
-                <input type ='text' name="searchDate" placeholder="Fecha"/>
-            </td>
-            <td>
-                <select>
-                    <option value="0">Evento</option>
-                    <option value="1">Actividad</option>
-                </select>
-            </td>
-            <td>
-                <input type="submit" name="create" value="Crear"/> 
-            </td>
-            <td>
-            </td>
-        </tr>
-    </form>
+    <tr>
+        <td>
+            <input type ='text' id="searchGeneral" placeholder="Busqueda General"/>
+        </td>
+        <td>
+            <input type ='text' id="searchDate" placeholder="Fecha"/>
+        </td>
+        <td>
+            <select id="typeActivity"style="width: 100%;">
+                <option value="0">Evento</option>
+                <option value="1">Actividad</option>
+            </select>
+        </td>
+        <td>
+            <input type="text" id="searchPlays" placeholder="Lugar">
+        </td>
+        <td>
+            <input type="datetime" id="searchHour" placeholder="Hora">
+        </td>
+        <td>
+            <input type="text" id="searchActor" placeholder="Actor">
+        </td>
+        <td>
+            <input type="button" id="search" value="Buscar"/> 
+        </td>
+        <td>
+        </td>
+    </tr>
 </table>
+<script>
+    $("#search").click(function () {
+        var args = {
+            'typeActivity': $('#typeActivity').val(),
+            'searchGeneral': $('#searchGeneral').val(),
+            'searchData': $('#searchDate').val(),
+            'searchPlays': $('#searchPlays').val(),
+            'searchActor': $('#searchActor').val()
+        };
 
+        $.post('../business/SearchBusiness.php', args, function () {
+
+
+
+        });
+    });
+</script>
 <tr>
     <td></td>
     <td>
-        <?php
-        if (isset($_GET['error'])) {
-            if ($_GET['error'] == "empty") {
-                echo '<p style = "color: red">Campo(s) vacio(s)</p>';
-            } else if ($_GET['error'] == "format") {
-                echo '<p style = "color: red">Error, formato de numero</p>';
-            } else if ($_GET['error'] == "dbError") {
-                echo '<center><p style = "color: red">Error al procesar la transacción</p></center>';
-            }
-        } else if (isset($_GET['success'])) {
-            echo '<p style = "color: green">Transacción realizada</p>';
-        }
-        ?>
+        <div id="message"></div>
     </td>
 </tr>
 
 <?php
+
 include_once '../public/footer.php';
-?>
