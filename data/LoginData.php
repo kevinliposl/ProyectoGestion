20587fe328dfc501071405c1a9209689f369928c
kevinliposl <kevinliposl@gmail.com>
2018-02-26 10:57:25 -0600

    asd

--- data/LoginData.php
+++ data/LoginData.php
@@ -12,7 +12,7 @@ class LoginData {
     function authenticate(Login $login) {
         $profile = array('student' => 'student', 'professor' => 'professor', 'administrative' => 'administrative', 'admin' => 'admin');
         foreach ($profile as $var) {
-            $query = $this->db->prepare("SELECT '" . $var . "' type, ac.*,tb.* FROM tbactor ac INNER JOIN tb" . $var . " tb ON ac.actorid = tb." . $var . "id WHERE ac.actormail=:mail AND tb." . $var . "password=:pass;");
+            $query = $this->db->prepare("SELECT '" . $var . "' type, ac.*,tb.* FROM tbactor ac INNER JOIN tb" . $var . " tb ON ac.actorid = tb." . $var . "id WHERE ac.actormail=:mail AND tb." . $var . "password=:pass AND " . $var . "state=1;");
             $query->execute(array('mail' => $login->getLoginMail(), 'pass' => $login->getLoginPassword()));
             $result = $query->fetch();
             $query->closeCursor();
@@ -26,7 +26,7 @@ class LoginData {
     function recoverPassword(Login $login) {
         $profile = array('student' => 'student', 'professor' => 'professor', 'administrative' => 'administrative', 'admin' => 'admin');
         foreach ($profile as $var) {
-            $query = $this->db->prepare("SELECT '" . $var . "' type, tb." . $var . "password, ac.actormail FROM tbactor ac INNER JOIN tb" . $var . " tb ON ac.actorid = tb." . $var . "id WHERE ac.actormail=:mail;");
+            $query = $this->db->prepare("SELECT '" . $var . "' type, tb." . $var . "password, ac.actormail FROM tbactor ac INNER JOIN tb" . $var . " tb ON ac.actorid = tb." . $var . "id WHERE ac.actormail = :mail " . $var . "state=1;");
             $query->execute(array('mail' => $login->getLoginMail()));
             $result = $query->fetch();
             $query->closeCursor();
@@ -36,40 +36,4 @@ class LoginData {
         }
         return $result;
     }
-
-//    function studentLogin(Login $login) {
-//        $query = $this->db->prepare("select ac.*, st.*, 'student' as type from tbactor as ac, tbstudent as st where ac.actorid=st.studentid and st.studentpassword='" . $login->getLoginPassword() . "' and ac.actormail='" . $login->getLoginMail() . "';");
-//        $query->execute();
-//        $result = $query->fetch();
-//        $query->closeCursor();
-//
-//        return $result;
-//    }
-//
-//    function professorLogin(Login $login) {
-//        $query = $this->db->prepare("select ac.*, pr.*, 'professor' as type from tbactor as ac, tbprofessor as pr where ac.actorid=pr.professorid and pr.professorpassword='" . $login->getLoginPassword() . "' and ac.actormail='" . $login->getLoginMail() . "';");
-//        $query->execute();
-//        $result = $query->fetch();
-//        $query->closeCursor();
-//
-//        return $result;
-//    }
-//
-//    function administrativeLogin(Login $login) {
-//        $query = $this->db->prepare("select ac.*, ad.*, 'administrative' as type from tbactor as ac, tbadministrative as ad where ac.actorid=ad.administrativeid and ad.administrativepassword='" . $login->getLoginPassword() . "' and ac.actormail='" . $login->getLoginMail() . "';");
-//        $query->execute();
-//        $result = $query->fetch();
-//        $query->closeCursor();
-//
-//        return $result;
-//    }
-//
-//    function admLogin(Login $login) {
-//        $query = $this->db->prepare("select ac.*, ad.*, 'adm' as type from tbactor as ac, tbadm as ad where ac.actorid=ad.adminid and ad.password='" . $login->getLoginPassword() . "' and ac.actormail='" . $login->getLoginMail() . "';");
-//        $query->execute();
-//        $result = $query->fetch();
-//        $query->closeCursor();
-//
-//        return $result;
-//    }
 }
