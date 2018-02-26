b4d46e746e5b492c20a3071e7796e367b7d8c62d
kevinliposl <kevinliposl@gmail.com>
2018-02-26 08:29:07 -0600

    asd

--- backup/catalogoCarreras.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `tbuniversity` VALUES (1,'UCR',1,1,1),(2,'UNA',1,1,1),(3,'TEC',1,1,1),(6,'INA',1,1,0);
-INSERT INTO `tbheadquarter` VALUES (1,'San Jose',1),(2,'Cartago',2),(3,'San jose',3),(4,'Cartago',3);
-INSERT INTO `tbenclosure` VALUES (1,'Heredia',0,6,1),(2,'San Pedro',1,1,1),(3,'La Sabana',1,1,1),(4,'Pavas',1,1,1);
