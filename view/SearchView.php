40acad7d964ef11208994952c84425c4983bd5f5
dylan <dylan@dylan-Inspiron-3437>
2018-02-27 12:27:58 -0600

    ..

--- view/SearchView.php
+++ view/SearchView.php
@@ -61,9 +61,8 @@ if (!isset(SSession::getInstance()->user)) {
         $('#message').text('Espere...');
 
         $.post('../business/SearchBusiness.php', args, function (data) {
-            $('#message').html("<a href='#'>Evento</a> ");//JSON.stringify(data.result));
             for (var dat in data.result) {
-                $('#message').text();
+                $('#message').html("<li><a href='CommentView.php?id=" + dat.activityid + "&title="+ dat.activitytitle+ "&des= " + dat.activitydescription+ "'><div> Fecha de creacion: " + dat.activitycreateddate+ ", Fecha de actualizacion: " + dat.activityupdatedate+ ", Numero de seguidores: " + dat.activitylikecount+ ", Cantidad de comentarios: " + dat.activitycommentcount+", Titulo: " + dat.activitytitle+ ", Descripcion: " + dat.activitydescription+ ", Lugar: " + dat.eventplace+ ", Dia: " + dat.eventdate+ ", Hora: " + dat.eventhour+ "</div></a></li>");
             }
         }, 'json').fail(function () {
             alert('Error al acceder al servidor');
