774dcb1bb9b0acf738903f77512db6059d382501
kevinliposl <kevinliposl@gmail.com>
2018-02-27 16:31:08 -0600

    awd

--- index.php
+++ index.php
@@ -27,54 +27,56 @@ if (isset(SSession::getInstance()->user)) {
                         <li><a href="view/LoginView.php"><div>Iniciar Sesi&oacute;n</div></a></li>
                         <?php
                     }if (isset(SSession::getInstance()->user)) {
-                        if (SSession::getInstance()->user['type'] == "admin") {
-                            ?>
-                            <li><a href="#"><div>Usuario</div></a>
-                                <ul>
-                                    <li><a href="view/StudentView.php"><div>Gesti&oacute;n de Estudiantes</div></a></li>
-                                    <li><a href="view/ProfessorView.php"><div>Gesti&oacute;n de Profesores</div></a></li>
-                                    <li><a href="view/AdministrativeView.php"><div>Gesti&oacute;n de Administrativos</div></a></li>
+                        if (SSession::getInstance()->user['actorchangedpassword'] !== 0) {
+                            if (SSession::getInstance()->user['type'] == "admin") {
+                                ?>
+                                <li><a href="#"><div>Usuario</div></a>
+                                    <ul>
+                                        <li><a href="view/StudentView.php"><div>Gesti&oacute;n de Estudiantes</div></a></li>
+                                        <li><a href="view/ProfessorView.php"><div>Gesti&oacute;n de Profesores</div></a></li>
+                                        <li><a href="view/AdministrativeView.php"><div>Gesti&oacute;n de Administrativos</div></a></li>
 
-                                </ul>
-                            </li>
-                            <li><a href="#"><div>Universidades</div></a>
-                                <ul>
-                                    <li><a href="view/CareerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
-                                    <li><a href="view/UniversityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
-                                    <li><a href="view/AdministrationInstallationView.php"><div>Gesti&oacute;n de Sedes y Recintos</div></a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><div>Actividades</div></a>
-                                <ul>
-                                    <li><a href="view/AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
-                                    <li><a href="view/EventView.php"><div>Ver Eventos</div></a></li>
-                                    <li><a href="view/AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
-                                    <li><a href="view/PostView.php"><div>Ver Publicaciones</div></a></li>
-                                </ul>
-                            </li>
-                            <li><a href="view/AdministrativeCommentView.php"><div>Gesti&oacute;n de Comentarios</div></a></li> 
+                                    </ul>
+                                </li>
+                                <li><a href="#"><div>Universidades</div></a>
+                                    <ul>
+                                        <li><a href="view/CareerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
+                                        <li><a href="view/UniversityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
+                                        <li><a href="view/AdministrationInstallationView.php"><div>Gesti&oacute;n de Sedes y Recintos</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><div>Actividades</div></a>
+                                    <ul>
+                                        <li><a href="view/AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
+                                        <li><a href="view/EventView.php"><div>Ver Eventos</div></a></li>
+                                        <li><a href="view/AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
+                                        <li><a href="view/PostView.php"><div>Ver Publicaciones</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="view/AdministrativeCommentView.php"><div>Gesti&oacute;n de Comentarios</div></a></li> 
 
-                            <li><a href="view/OrganChartView.php"><div>Organigrama</div></a></li> 
-                            <li><a href="view/SearchView.php"><div>Busqueda</div></a></li>
-                        <?php } else { ?>
-                            <li><a href="#"><div>Usuario</div></a>
-                                <ul>
-                                    <li><a href="view/ProfileView.php"><div>Perfil</div></a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><div>Actividades</div></a>
-                                <ul>
-                                    <li><a href="view/AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
-                                    <li><a href="view/EventView.php"><div>Ver Eventos</div></a></li>
-                                    <li><a href="view/AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
-                                    <li><a href="view/PostView.php"><div>Ver Publicaciones</div></a></li>
-                                </ul>
-                            </li>
-                            <li><a href="view/OrganChartView.php"><div>Organigrama</div></a></li> 
-                            <li><a href="view/SearchView.php"><div>Busqueda</div></a></li>
-                        <?php } ?>
-                        <li><a href="business/LoginBusiness.php?signout='i'"><div>Cerrar Sesi&oacute;n</div></a></li>
-                    <?php } ?>
+                                <li><a href="view/OrganChartView.php"><div>Organigrama</div></a></li> 
+                                <li><a href="view/SearchView.php"><div>Busqueda</div></a></li>
+                            <?php } else { ?>
+                                <li><a href="#"><div>Usuario</div></a>
+                                    <ul>
+                                        <li><a href="view/ProfileView.php"><div>Perfil</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><div>Actividades</div></a>
+                                    <ul>
+                                        <li><a href="view/AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
+                                        <li><a href="view/EventView.php"><div>Ver Eventos</div></a></li>
+                                        <li><a href="view/AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
+                                        <li><a href="view/PostView.php"><div>Ver Publicaciones</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="view/OrganChartView.php"><div>Organigrama</div></a></li> 
+                                <li><a href="view/SearchView.php"><div>Busqueda</div></a></li>
+                            <?php } ?>
+                            <li><a href="business/LoginBusiness.php?signout='i'"><div>Cerrar Sesi&oacute;n</div></a></li>
+                        <?php }
+                    } ?>
                 </ul>
             </nav>
         </header>
--- public/header.php
+++ public/header.php
@@ -2,6 +2,7 @@
 require_once '../util/RandomPassGenerator.php';
 require_once '../util/SSession.php';
 RandomPassGenerator::getInstance();
+
 ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="es">
@@ -21,53 +22,58 @@ RandomPassGenerator::getInstance();
                     <?php if (!isset(SSession::getInstance()->user)) { ?>
                         <li><a href="RegisterView.php"><div>Registro</div></a></li>
                         <li><a href="LoginView.php"><div>Iniciar Sesi&oacute;n</div></a></li>
-                    <?php }if (isset(SSession::getInstance()->user)) {
-                        if(SSession::getInstance()->user['type'] == "adm"){?>
-                        <li><a href="#"><div>Usuarios</div></a>
-                            <ul>
-                                <li><a href="StudentView.php"><div>Gesti&oacute;n de Estudiantes</div></a></li>
-                                <li><a href="ProfessorView.php"><div>Gesti&oacute;n de Profesores</div></a></li>
-                                <li><a href="AdministrativeView.php"><div>Gesti&oacute;n de Administrativos</div></a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#"><div>Universidades</div></a>
-                            <ul>
-                                <li><a href="CareerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
-                                <li><a href="UniversityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
-                                <li><a href="AdministrationInstallationView.php"><div>Gesti&oacute;n de Sedes y Recintos</div></a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#"><div>Actividades</div></a>
-                            <ul>
-                                <li><a href="AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
-                                <li><a href="EventView.php"><div>Ver Eventos</div></a></li>
-                                <li><a href="AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
-                                <li><a href="PostView.php"><div>Ver Publicaciones</div></a></li>
-                                <li><a href="AdministrativeCommentView.php"><div>Gesti&oacute;n de Comentarios</div></a></li>
+                        <?php
+                    }if (isset(SSession::getInstance()->user)) {
+                        if (SSession::getInstance()->user['actorchangedpassword'] !== 0) {
+                            if (SSession::getInstance()->user['type'] == "admin") {
+                                ?>
+                                <li><a href="#"><div>Usuarios</div></a>
+                                    <ul>
+                                        <li><a href="StudentView.php"><div>Gesti&oacute;n de Estudiantes</div></a></li>
+                                        <li><a href="ProfessorView.php"><div>Gesti&oacute;n de Profesores</div></a></li>
+                                        <li><a href="AdministrativeView.php"><div>Gesti&oacute;n de Administrativos</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><div>Universidades</div></a>
+                                    <ul>
+                                        <li><a href="CareerView.php"><div>Gesti&oacute;n de Carreras</div></a></li>
+                                        <li><a href="UniversityView.php"><div>Gesti&oacute;n de Universidades</div></a></li>
+                                        <li><a href="AdministrationInstallationView.php"><div>Gesti&oacute;n de Sedes y Recintos</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><div>Actividades</div></a>
+                                    <ul>
+                                        <li><a href="AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
+                                        <li><a href="EventView.php"><div>Ver Eventos</div></a></li>
+                                        <li><a href="AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
+                                        <li><a href="PostView.php"><div>Ver Publicaciones</div></a></li>
+                                        <li><a href="AdministrativeCommentView.php"><div>Gesti&oacute;n de Comentarios</div></a></li>
 
-                            </ul>
-                        </li>
-                        <li><a href="OrganChartView.php"><div>Organigrama</div></a></li> 
-                        <li><a href="SearchView.php"><div>Busqueda</div></a></li>
-                        <?php }else{?>
-                            <li><a href="#"><div>Usuario</div></a>
-                            <ul>
-                                <li><a href="ProfileView.php"><div>Perfil</div></a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#"><div>Actividades</div></a>
-                            <ul>
-                                <li><a href="AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
-                                <li><a href="EventView.php"><div>Ver Eventos</div></a></li>
-                                <li><a href="AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
-                                <li><a href="PostView.php"><div>Ver Publicaciones</div></a></li>
-                            </ul>
-                        </li>
-                        <li><a href="OrganChartView.php"><div>Organigrama</div></a></li> 
-                        <li><a href="SearchView.php"><div>Busqueda</div></a></li>
-                        <?php }?>
-                        <li><a href="../business/LoginBusiness.php?signout=''"><div>Cerrar Sesi&oacute;n</div></a></li>
-                    <?php } ?>
+                                    </ul>
+                                </li>
+                                <li><a href="OrganChartView.php"><div>Organigrama</div></a></li> 
+                                <li><a href="SearchView.php"><div>Busqueda</div></a></li>
+                            <?php } else { ?>
+                                <li><a href="#"><div>Usuario</div></a>
+                                    <ul>
+                                        <li><a href="ProfileView.php"><div>Perfil</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><div>Actividades</div></a>
+                                    <ul>
+                                        <li><a href="AdministrativeEventView.php"><div>Gesti&oacute;n de Eventos</div></a></li>
+                                        <li><a href="EventView.php"><div>Ver Eventos</div></a></li>
+                                        <li><a href="AdministrativePostView.php"><div>Gesti&oacute;n de Publicaciones</div></a></li>
+                                        <li><a href="PostView.php"><div>Ver Publicaciones</div></a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="OrganChartView.php"><div>Organigrama</div></a></li> 
+                                <li><a href="SearchView.php"><div>Busqueda</div></a></li>
+                            <?php } ?>
+                            <li><a href="../business/LoginBusiness.php?signout=''"><div>Cerrar Sesi&oacute;n</div></a></li>
+                        <?php }
+                    }
+                    ?>
                 </ul>
             </nav>
         </header>
